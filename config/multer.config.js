8e73d3dd3439c15e0320d43828d1cc381369ec12
AshvielC <106778498+AshvielC@users.noreply.github.com>
2024-11-25 08:34:54 +1200

    Update multer.config.js

--- config/multer.config.js
+++ config/multer.config.js
@@ -1,7 +1,7 @@
 'use strict'
 
 import multer from 'multer'
-const imgPath = '/mnt/data/uploads'
+const imgPath = '/mnt/data/uploads/'
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, imgPath);
