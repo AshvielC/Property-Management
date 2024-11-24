079f10e88cc6506b569c3e1805089e5d87c21c55
AshvielC <106778498+AshvielC@users.noreply.github.com>
2024-11-25 08:31:21 +1200

    Update multer.config.js
    changed upload path

--- config/multer.config.js
+++ config/multer.config.js
@@ -4,7 +4,7 @@ import multer from 'multer'
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, './uploads/');
+        cb(null, '/mnt/data/uploads/');
     },
     filename: (req, file, cb) => {
         cb(null, Date.now() + '-' + file.originalname); // File naming convention
