7463e6a9387b582adf3336dfa1db6d175afcda11
AshvielC <106778498+AshvielC@users.noreply.github.com>
2024-11-25 08:27:00 +1200

    Update staff.controller.js
    changed staff image path to /mnt/data/uploads

--- controllers/staff.controller.js
+++ controllers/staff.controller.js
@@ -28,7 +28,7 @@ const staffController = {
         let convertedPath = profilePic.path.replace(/\\/g, '')
             .replace(/^uploads/g, '');
 
-        convertedPath =   'https://property-management-production-63a4.up.railway.app/uploads' +  convertedPath;
+        convertedPath =   'https://property-management-production-63a4.up.railway.app/mnt/data/uploads' +  convertedPath;
         //console.log(convertedPath)
         try {
             // Check if a staff member with the same phone number already exists
